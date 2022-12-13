cdcfe0245fee55db77b17ddf6b1ba7103358ae5c
Saptyadeep <bsaptyadeep@gmail.com>
2022-12-13 21:24:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './App.css';
@@ -16,11 +16,14 @@ function App() {
     branch: "",
   })
 
+  const [error, setError] = useState("");
+  const [msg, setMsg] = useState("");
+
   const [file, setFile] = useState("")
 
   const handleChange = ({ currentTarget: input}) => {
     setData({ ...data, [input.name] : input.value})
-    console.log(data)
+   // console.log(data)
   }
 
   const handleChangeFile = (e) => {
@@ -32,28 +35,54 @@ function App() {
       const formData = new FormData();
       formData.append('file', file)
       formData.append('data', JSON.stringify(data))
-      const res = await axios.post("https://question-papers-coding-junction.onrender.com/api/question-paper", formData)
-      console.log(res)
+      const res = await axios.post("https://question-paper-nodejs-e3e7.vercel.app/api/question-paper", formData)
+     // console.log(res)
+      setMsg(res);
     }catch(err){
-      console.log(err)
+      setError(err)
+     // console.log(err)
     }
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setError("");
+      setMsg("")
+    }, 4000);
+  
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, [])
+  
 
   return (
     <div className="App">
       <h1>Upload Question Paper</h1>
+      {
+        msg?
+        (
+          <div className="message">{msg.data}
+          </div>
+        ):""
+
+      }
+      {
+        error?
+        (
+          <div className="error">{error.message}</div>
+        ):""
+      }
       <Form>
       <Form.Group className="mb-3" controlId="formBasicSubjectCode">
-        <Form.Control required name="subjectCode" value={data.subjectCode} onChange={handleChange} type="text" placeholder="Enter Subject Code" />
+        <Form.Control required name="subjectCode" value={data.subjectCode||""} onChange={handleChange} type="text" placeholder="Enter Subject Code" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicSubjectName">
-        <Form.Control required name="subjectName" value={data.subjectName} onChange={handleChange} type="text" placeholder="Enter Subject Name" />
+        <Form.Control required name="subjectName" value={data.subjectName||""} onChange={handleChange} type="text" placeholder="Enter Subject Name" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicYear">
-        <Form.Control required name="year" value={data.value} onChange={handleChange} type="text" placeholder="Enter Subject Year" />
+        <Form.Control required name="year" value={data.year||""} onChange={handleChange} type="text" placeholder="Enter Subject Year" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicYear">
-      <Form.Select required name="exam" value={data.exam} onChange={handleChange} aria-label="Select Exam">
+      <Form.Select required name="exam" value={data.exam||""} onChange={handleChange} aria-label="Select Exam">
       <option>Select Exam</option>
       <option value="MidSem">MidSem</option>
       <option value="Sem">Sem</option>
@@ -74,7 +103,7 @@ function App() {
       </Form.Group>
       <Form.Group className="mb-3">
       <Form.Select required name="branch" value={data.branch} onChange={handleChange} aria-label="Select Branch">
-      <option>Select Brach</option>
+      <option>Select Branch</option>
       <option value="CSE">CSE</option>
       <option value="IT">IT</option>
       <option value="ECE">ECE</option>
